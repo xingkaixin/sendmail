25e27ee3678ec3a1622681e80ba024e64b512156
XingKaiXin <xingkaixin@gmail.com>
2016-03-08 17:38:48 +0800

    update

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-
+import cx_Oracle
+import os
+import arrow
 from flask.ext.mail import (
     Mail,
     Message
@@ -10,6 +12,115 @@ from flask import (
     render_template
 )
 from democonf import BasicConfig
+
+
+class DbConn(object):
+
+    db = None
+
+    def __init__(self, conn_str):
+        self.db = cx_Oracle.connect(conn_str)
+
+    def close(self):
+        self.db.close()
+        self.db = None
+
+    def commit(self):
+        self.db.commit()
+
+    def rollback(self):
+        self.db.rollback()
+
+    def query(self, sql, val={}):
+        cur = self.db.cursor()
+        cur.execute(sql, val)
+        rs = cur.fetchall()
+        cur.close()
+        return rs
+
+    def querybyGenerator(self, sql, val={}):
+        cur = self.db.cursor()
+        cur.execute(sql, val)
+        while cur:
+            yield cur.next()
+        cur.close()
+
+    def execute(self, sql, val={}):
+        cur = self.db.cursor()
+        cur.execute(sql, val)
+        counts = cur.rowcount
+        cur.close()
+        return counts
+
+    def callProc(self, proc, *args):
+        cur = self.db.cursor()
+        cur.callproc(proc, *args)
+        cur.close()
+        return True
+
+
+def updateSeq(dbconn):
+    proc = 'PKG_11_SHOP_UA_REPORT_HOUR.SP_MAIN'
+    dbconn.callProc(proc, (None, None))
+    return True
+
+
+def run_data():
+    DWCONN = os.getenv('DWCONN')
+    try:
+        dc = DbConn(DWCONN)
+        updateSeq(dc)
+    except Exception as e:
+        print e
+        dc.rollback()
+    finally:
+        dc.close()
+
+
+def get_data(dayid, hour):
+    DWCONN = os.getenv('DWCONN')
+    try:
+        dc = DbConn(DWCONN)
+        sql  = """
+        SELECT SHOP_NAME,
+               PERIOD,
+               TRIM(TO_CHAR(SUM(ORDERS), '999,999,999')) ORDERS,
+               ROUND(SUM(REVENUE)) REVENUE,
+               TRIM(TO_CHAR(SUM(TTL_ORDERS), '999,999,999')) TTL_ORDERS,
+               ROUND(SUM(TTL_REVENUE)) TTL_REVENUE
+          FROM DWH_RPT_UA_HOURLY      A,
+               DWH_RPT_REFERENCE_CODE B
+         WHERE DAYID = {dayid}
+           AND HOUR = {hour}
+           AND B.TYPE_ID = 6
+           AND A.SHOP_ID = B.MAPPING_FROM1
+         GROUP BY B.MAPPING_TO,
+                  SHOP_NAME,
+                  PERIOD
+         ORDER BY B.MAPPING_TO
+        """.format(dayid=dayid, hour=hour)
+
+        rs = dc.query(sql)
+        shops = []
+        for r in rs:
+            shop = {
+                'shop': r[0],
+                'time': r[1],
+                'order': r[2],
+                'revenue': r[3],
+                'ttl_order': r[4],
+                'ttl_revenue': r[5],
+                'sum': True if r[0] == 'Sum' else False
+            }
+            shops.append(shop)
+        return shops
+
+    except Exception as e:
+        print e
+        dc.rollback()
+    finally:
+        dc.close()
+
 
 mail = Mail()
 
@@ -25,25 +136,18 @@ def sendmail():
     title = 'UA TTL'
     reciver = 'dingjiao.lin@baozun.cn'
 
-    shops = []
-    shop = {
-        'shop': 'TMall',
-        'time': '09:00-10:00',
-        'order': 10,
-        'revenue': 10000,
-        'ttl_order': 100,
-        'ttl_revenue': 100000
-    }
-    shops.append(shop)
-    shop = {
-        'shop': 'JD',
-        'time': '09:00-10:00',
-        'order': 5,
-        'revenue': 5000,
-        'ttl_order': 50,
-        'ttl_revenue': 50000
-    }
-    shops.append(shop)
+    DWCONN = os.getenv('DWCONN')
+    run_data()
+
+    batchday = arrow.utcnow().to('local')
+    dayid = batchday.format('YYYYMMDD')
+    hour = batchday.format('HH')
+
+    if hour == '00':
+        hour = '23'
+        dayid = batchday.replace(days=-1).format('YYYYMMDD')
+
+    shops = get_data(dayid, hour)
 
     msg = Message(title)
     msg.add_recipient(reciver)
